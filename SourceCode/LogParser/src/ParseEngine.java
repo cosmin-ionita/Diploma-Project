77c3152bdcb9cd4664654811d4749330956c1b7b
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-03-13 15:13:50 +0200

    Add data model

--- SourceCode/LogParser/src/ParseEngine.java
+++ SourceCode/LogParser/src/ParseEngine.java
@@ -1,8 +1,16 @@
+import Models.SolrDataModel;
+
 import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 
 public class ParseEngine {
+
+    private static void processFile(File file) {
+        SolrDataModel model = new SolrDataModel();
+
+        
+    }
 
     public static void ParseArchive(Path path) {
 
@@ -14,7 +22,7 @@ public class ParseEngine {
                 List<File> files = DecompressService.decompressArchive(path);
 
                 for(File file : files) {
-
+                    processFile(file);
                 }
             }
         });
