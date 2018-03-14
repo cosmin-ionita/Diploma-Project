b298407c2e117a10846ba6764bb75f6b86fba8a0
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-03-14 13:20:56 +0200

    Refactor DecompressorFactory class

--- SourceCode/LogParser/src/DecompressorFactory.java
+++ SourceCode/LogParser/src/DecompressorFactory.java
@@ -4,8 +4,17 @@ import Decompressors.ZipDecompressor;
 import Enums.ArchiveTypes;
 import Interfaces.IDecompressable;
 
+/**
+ * This class is a simple implementation of factory pattern for decompressors
+ */
 public class DecompressorFactory {
 
+    /**
+     * This method simply takes an archive type and returns the appropriate decompressor object
+     *
+     * @param archiveType - the type of the archive based on it's extension
+     * @return - returns the specialized decompressor
+     */
     public IDecompressable getDecompressor(ArchiveTypes archiveType) {
 
         if(archiveType == ArchiveTypes.TAR)
