fff0544dc91f986293124a02a4f0bdbd2a6bac94
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-03-14 13:19:41 +0200

    Rector DecompressService class

--- SourceCode/LogParser/src/DecompressService.java
+++ SourceCode/LogParser/src/DecompressService.java
@@ -1,31 +1,33 @@
-import Decompressors.TarDecompressor;
 import Enums.ArchiveTypes;
 import Interfaces.IDecompressable;
-import javafx.scene.shape.Arc;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *  This class is just a controller for decompressors.
+ */
 public class DecompressService {
 
-    /* This should go into an utils-like module / class */
-    private static String getExtension(String fileName) {
-        return fileName.split(".")[1];
-    }
-
-    public static List<File> decompressArchive(Path path) {
+    /**
+     * This method takes a path to an archive, retrieves the extension, takes the appropriate decompressor and uses it
+     * to obtain the files inside the archive.
+     *
+     * @param archivePath - the path to the archive (this comes directly from the WatchEngine)
+     * @return - the list of files contained in the archive
+     */
+    public static List<File> decompressArchive(Path archivePath) {
 
         List<File> files;
         DecompressorFactory factory = new DecompressorFactory();
 
-        String extension = getExtension(path.toString());
+        String extension = Utils.getExtension(archivePath.toString());
 
         IDecompressable decompressor = factory.getDecompressor(ArchiveTypes.valueOf(extension));
         assert decompressor != null;
 
-        files = decompressor.decompress(path.toFile());
+        files = decompressor.decompress(archivePath.toFile());
 
         return files;
     }
