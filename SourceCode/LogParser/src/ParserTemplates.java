0107574a0e9f005a2589ce12c57880011f43b36c
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-03-14 13:15:59 +0200

    Add ParserTemplates class

--- SourceCode/LogParser/src/ParserTemplates.java
+++ SourceCode/LogParser/src/ParserTemplates.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import Models.SolrDataModel;
 
+/**
+ * This class provides a set of public methods each one corresponding to a parser template. All the methods implemented
+ * here will be tried one by one until a line is correctly parsed into a model.
+ */
 public class ParserTemplates {
+
+    public static SolrDataModel log4jTemplate(String line) {
+        return null;
+    }
+
+    public static SolrDataModel anotherTemplate(String line) {
+        return null;
+    }
 }
