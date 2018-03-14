c58254eccd809405e4ac90db27904e1fc70096bd
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-03-14 13:16:26 +0200

    Add Utils class

--- SourceCode/LogParser/src/Utils.java
+++ SourceCode/LogParser/src/Utils.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import Models.SolrDataModel;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Utils {
+
+    public static String getExtension(String fileName) {
+        return fileName.split(".")[1];
+    }
+
+    public static boolean checkModel(SolrDataModel model) {
+
+        if(     model.getDate()           != null &&
+                model.getHostName()       != null &&
+                model.getJobId()          != null &&
+                model.getLogLevel()       != null &&
+                model.getMessage()        != null &&
+                model.getSourceFileName() != null &&
+                model.getTime()           != null)
+
+            return true;
+
+        return false;
+    }
+
+    public static void updateHostname(SolrDataModel model) {
+        try{
+            InetAddress host = InetAddress.getLocalHost();
+            model.setHostName(host.getHostName());
+        }
+        catch (UnknownHostException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void updateSourceFileName(SolrDataModel model, File file) {
+            model.setSourceFileName(file.getName());
+    }
 }
