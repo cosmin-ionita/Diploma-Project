a80f852f48fb111dc6e564102362082b99dbaf98
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-03-20 10:26:53 +0200

    Refactor ArchiveTypes enum

--- SourceCode/LogParser/src/Enums/ArchiveTypes.java
+++ SourceCode/LogParser/src/Enums/ArchiveTypes.java
@@ -1,18 +1,7 @@
 package Enums;
 
 public enum ArchiveTypes {
-    TAR (".tar"),
-    ZIP (".zip"),
-    GZIP (".gzip");
-
-
-    private String archiveExtension;
-
-    ArchiveTypes(String archiveExtension) {
-        this.archiveExtension = archiveExtension;
-    }
-
-    public String toString(ArchiveTypes type) {
-        return type.archiveExtension;
-    }
+    TAR,
+    ZIP,
+    GZIP
 }
