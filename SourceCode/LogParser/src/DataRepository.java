70baa0b82d724f2e199971ffb46c4011d803cfcc
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-03-20 10:20:27 +0200

    Add file generate method on DataRepository

--- SourceCode/LogParser/src/DataRepository.java
+++ SourceCode/LogParser/src/DataRepository.java
@@ -1,5 +1,10 @@
 import Models.SolrDataModel;
+import flexjson.JSONSerializer;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,12 +15,41 @@ import java.util.List;
  */
 public class DataRepository {
 
-    private static int models_per_file = 10;
+    private static int models_per_file = 100;
+
+    private static int file_count = 0;
+    private static String file_prefix = "generated_";
+    private static String destination_directory = "/Users/ioni/streaming_logs/generated/";
 
     private static List<SolrDataModel> models = new ArrayList<>();
 
     private static void generateFile() {
-            /* Creates a file and puts all models in there */
+
+        /* Take each model and write it in a file */
+
+        List<SolrDataModel> back_models = new ArrayList<>(models);
+
+        JSONSerializer serializer = new JSONSerializer();
+
+        String serialized_models = serializer.serialize(back_models);
+
+        String fileName = destination_directory + file_prefix + file_count++;
+
+        File file = new File(fileName);
+
+        try {
+
+            FileOutputStream outputStream = new FileOutputStream(file);
+
+            outputStream.write(serialized_models.getBytes());
+
+            outputStream.close();
+
+            Logger.out("File generated: " + fileName + " with " + back_models.size() + " models serialized.");
+
+        }catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public static void addModel(SolrDataModel model) {
@@ -26,5 +60,21 @@ public class DataRepository {
 
             models = new ArrayList<>();
         }
+    }
+
+    public static void setDestinationDirectory(String path) {
+        destination_directory = path;
+    }
+
+    /**
+     * This method simply generate the models file with the existing models stored in models list.
+     *
+     * It is called at the end of the parsing stage in order to flush the accumulated models (probably less
+     * than models_per_file)
+     */
+    public static void dispatchModels() {
+        generateFile();
+
+        models = new ArrayList<>();
     }
 }
