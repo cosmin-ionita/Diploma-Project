d9206e65baf64e7c3f6bf1b5d858de3e0f5cbca6
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-05-02 11:01:17 +0300

    Add deploy scripts

---
+++ SourceCode/DeployScripts/deploy_logGenerator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## This script deploys the log generator jar file to the cluster
+
+LOG_GEN_PATH=../../CodeSamples/LogGenerator/Log_Generator/out/artifacts/Log_Generator_jar/Log_Generator.jar
+CLUSTER_CONFIG=../../../../cns/clusters/kscluster-ioni.yaml
+REMOTE_PATH=/home/ec2-user/workDir/binaries/logGenerator
+NODE_NAME=va6-ioni-ksmaster-3
+
+echo "Deploy in progress..."
+
+ops $CLUSTER_CONFIG sync $LOG_GEN_PATH $NODE_NAME:$REMOTE_PATH -l ec2-user
+
+echo "Deploy done!"
