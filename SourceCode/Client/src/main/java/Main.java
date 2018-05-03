40eb1f5fcea9535e1435e54b2ca9f26905b4cdc6
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-05-03 18:23:35 +0300

    Add gui handling logic to Main method

--- SourceCode/Client/src/main/java/Main.java
+++ SourceCode/Client/src/main/java/Main.java
@@ -6,16 +6,20 @@ public class Main {
         System.out.println(StringsMapping.help);
     }
 
+    private static void showGui() {
+        GuiManager manager = new GuiManager();
+        manager.launchGui();
+    }
+
     public static void main(String args[]) {
 
         if(args[0].equals(StringsMapping.queryShort) || args[0].equals(StringsMapping.queryLong) && args.length > 2) {
             QueryManager manager = QueryManager.getInstance();
-
-
-
         }
         else if(args[0].equals(StringsMapping.commandShort)  || args[0].equals(StringsMapping.commandLong)) {
 
+        } else if(args[0].equals(StringsMapping.gui)) {
+            showGui();
         } else {
             printHelp();
         }
