71b38bb35f270139d0af8a685da28017711e6cfd
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-05-03 18:21:16 +0300

    Add gui command parameter

--- SourceCode/Client/src/main/java/Utils/StringsMapping.java
+++ SourceCode/Client/src/main/java/Utils/StringsMapping.java
@@ -8,5 +8,7 @@ public class StringsMapping {
     public final static String commandShort = "-c";
     public final static String commandLong = "--command";
 
+    public final static String gui = "--gui";
+
     public final static String help = "Usage: loGrep [-q | --query key1=value1,key2=value2,...] [-c | --command command]";
 }
