d2e39be7b35feda5945c3876e9250a92cea826f8
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-05-03 13:30:07 +0300

    Update the limit of files that need to accumulate before indexing

--- SourceCode/IndexTrigger/src/main/java/HDFS_Watch/HDFSWatcher.java
+++ SourceCode/IndexTrigger/src/main/java/HDFS_Watch/HDFSWatcher.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class HDFSWatcher {
 
-    private static final int indexFilesCount = 100;
+    private static final int indexFilesCount = 10;
 
     public static void main( String[] args ) throws IOException, InterruptedException, MissingEventsException
     {
