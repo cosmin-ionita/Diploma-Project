ef68b1a05a42093e8cf86e6cf4177eb0043dfebc
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-05-03 13:31:03 +0300

    Refactor code

--- SourceCode/LogParser/src/main/java/ModelBuffer.java
+++ SourceCode/LogParser/src/main/java/ModelBuffer.java
@@ -44,7 +44,7 @@ public class ModelBuffer {
 
             Logger.out("File generated: " + file.getName() + " with " + models.size() + " models serialized.");
 
-        }catch (IOException exception) {
+        } catch (IOException exception) {
             exception.printStackTrace();
         }
     }
