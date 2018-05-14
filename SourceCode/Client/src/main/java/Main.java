b9d2e04311eafe8bb4bc54a79599a79417760117
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-05-14 12:54:36 +0300

    Clean main class

--- SourceCode/Client/src/main/java/Main.java
+++ SourceCode/Client/src/main/java/Main.java
@@ -1,34 +1,53 @@
-import Gui.GuiMgr;
-import Utils.StringsMapping;
+import Executors.CommandExecutor;
+import Executors.QueryExecutor;
+import Managers.CommandManager;
+import Managers.QueryManager;
+import Models.Command;
+import Models.Query;
+import Utils.Utils;
+
+/**
+ *
+ *  user@host:$> loGrep [-q field1=value1 field2=value2 field3=value3]
+ *                      [-ti time1 time2]
+ *                      [-di date1 date2]
+ *                      [-obd asc/desc]
+ *                      [-obt asc/desc]
+ *                      [-e file.txt]
+ *
+ *  user@host:$> loGrep [--query field1=value1 field2=value2 field3=value3]
+ *                      [--time-interval time1 time2]
+ *                      [--date-interval date1 date2]
+ *                      [--order-by-date asc/desc]
+ *                      [--order-by-time asc/desc]
+ *                      [--export file.txt]
+ *
+ */
 
 public class Main {
 
-    private static void printHelp() {
-        System.out.println(StringsMapping.help);
-    }
-
-    private static void showGui() {
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                javafx.application.Application.launch(GuiMgr.class);
-            }
-        });
-
-        t.start();
-    }
 
     public static void main(String args[]) {
 
-        if(args[0].equals(StringsMapping.queryShort) || args[0].equals(StringsMapping.queryLong) && args.length > 2) {
+        if(Utils.isQuery(args)) {
             QueryManager manager = QueryManager.getInstance();
-        }
-        else if(args[0].equals(StringsMapping.commandShort)  || args[0].equals(StringsMapping.commandLong)) {
 
-        } else if(args[0].equals(StringsMapping.gui)) {
-            showGui();
+            Query query = manager.buildQuery(args);
+
+            QueryExecutor.executeQuery(query);
+
+        }
+        else if(Utils.isCommand(args)) {
+            CommandManager manager = CommandManager.getInstance();
+
+            Command command = manager.buildCommand(args);
+
+            CommandExecutor.executeCommand(command);
+
+        } else if(Utils.isGUI(args)) {
+            Utils.showGUI();
         } else {
-            printHelp();
+            Utils.printHelp();
         }
     }
 }
