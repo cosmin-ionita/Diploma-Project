89542a698801dfa7212a06eaa1ece67836bde6cc
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-05-14 12:52:12 +0300

    Add new option mappings

--- SourceCode/Client/src/main/java/Utils/StringsMapping.java
+++ SourceCode/Client/src/main/java/Utils/StringsMapping.java
@@ -5,10 +5,21 @@ public class StringsMapping {
     public final static String queryShort = "-q";
     public final static String queryLong = "--query";
 
+    public final static String timeIntervalLong = "--time-interval";
+    public final static String timeIntervalShort = "-ti";
+
+    public final static String dateIntervalLong = "--date-interval";
+    public final static String dateIntervalShort = "-di";
+
+    public final static String exportShort = "-e";
+    public final static String exportLong = "--export";
+
     public final static String commandShort = "-c";
     public final static String commandLong = "--command";
 
+
+
     public final static String gui = "--gui";
 
-    public final static String help = "Usage: loGrep [-q | --query key1=value1,key2=value2,...] [-c | --command command]";
+    public final static String help = "Usage: loGrep [-q | --query key1=value1 key2=value2,...] [-c | --command command]";
 }
