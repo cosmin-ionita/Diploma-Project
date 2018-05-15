71c552255ed274bac2dbff5ffc365cbee6e4bc47
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-05-15 14:20:03 +0300

    Update strings mapping
    Signed-off-by: Cosmin Ionita <ionita.cosmin@outlook.com>

--- SourceCode/Client/src/main/java/Utils/StringsMapping.java
+++ SourceCode/Client/src/main/java/Utils/StringsMapping.java
@@ -2,24 +2,40 @@ package Utils;
 
 public class StringsMapping {
 
-    public final static String queryShort = "-q";
-    public final static String queryLong = "--query";
-
-    public final static String timeIntervalLong = "--time-interval";
-    public final static String timeIntervalShort = "-ti";
-
-    public final static String dateIntervalLong = "--date-interval";
-    public final static String dateIntervalShort = "-di";
-
-    public final static String exportShort = "-e";
-    public final static String exportLong = "--export";
-
-    public final static String commandShort = "-c";
-    public final static String commandLong = "--command";
+    public final static String queryShort = "q";
+    public final static String queryLong = "query";
+    public final static String queryDescription = "Specify a list of key value pairs separated by space, like so: -q field1=value1 field2=value2";
 
 
+    public final static String timeIntervalShort = "ti";
+    public final static String timeIntervalLong = "time-interval";
+    public final static String timeIntervalDescription = "Specify 2 timestamps in any format that describe the interval you want to filter";
 
-    public final static String gui = "--gui";
+    public final static String dateIntervalShort = "di";
+    public final static String dateIntervalLong = "date-interval";
+    public final static String dateIntervalDescription = "Specify 2 dates in any format that describe the interval you want to filter";
 
-    public final static String help = "Usage: loGrep [-q | --query key1=value1 key2=value2,...] [-c | --command command]";
+    public final static String orderByDateShort = "obd";
+    public final static String orderByDateLong = "order-by-date";
+    public final static String orderByDateDescription = "Specify 'ASC' or 'DESC' depending on the order you want" ;
+
+    public final static String orderByTimeShort = "obt";
+    public final static String orderByTimeLong = "order-by-time";
+    public final static String orderByTimeDescription = "Specify 'ASC' or 'DESC' depending on the order you want" ;
+
+    public final static String exportShort = "e";
+    public final static String exportLong = "export";
+    public final static String exportDescription = "Specify the filename where you want the search results to be saved";
+
+    public final static String commandShort = "c";
+    public final static String commandLong = "command";
+
+    public final static String statusCommand = "status";
+
+    public final static String indexInterval = "index-interval";
+    public final static String dataRetention = "data-retention-policy";
+
+    public final static String guiShort = "g";
+    public final static String guiLong = "gui";
+    public final static String guiDescription = "This will launch the desktop GUI";
 }
