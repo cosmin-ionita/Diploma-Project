f396b39b1fba1eb61dc261be303ba7f3a496e4a7
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-06-28 16:35:50 +0300

    Refactor code

--- SourceCode/LogParser/src/main/java/Utils/Utils.java
+++ SourceCode/LogParser/src/main/java/Utils/Utils.java
@@ -98,27 +98,16 @@ public class Utils {
 
     public static boolean checkModel(SolrDataModel model) {
 
-        if(     model.getDate()           != null &&
+        if(     model.getTimeStamp()           != null &&
                 //model.getHostName()       != null &&
                 //model.getJobId()          != null &&
                 model.getLogLevel()       != null &&
-                model.getMessage()        != null &&
+                model.getMessage()        != null)
                 //model.getSourceFileName() != null &&
-                model.getTime()           != null)
 
             return true;
 
         return false;
-    }
-
-    public static void updateHostname(SolrDataModel model) {
-        try{
-            InetAddress host = InetAddress.getLocalHost();
-            model.setHostName(host.getHostName());
-        }
-        catch (UnknownHostException exception) {
-            exception.printStackTrace();
-        }
     }
 
     public static void updateSourceFileName(SolrDataModel model, File file) {
