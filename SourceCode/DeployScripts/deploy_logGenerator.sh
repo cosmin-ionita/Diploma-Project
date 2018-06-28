88db35539a0a4f8922e121eb11fb3f702ad445e2
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-06-28 16:23:49 +0300

    Reshape log generator deploy script

--- SourceCode/DeployScripts/deploy_logGenerator.sh
+++ SourceCode/DeployScripts/deploy_logGenerator.sh
@@ -3,9 +3,9 @@
 ## This script deploys the log generator jar file to the cluster
 
 LOG_GEN_PATH=../../CodeSamples/LogGenerator/Log_Generator/out/artifacts/Log_Generator_jar/Log_Generator.jar
-CLUSTER_CONFIG=../../../../cns/clusters/kscluster-ioni.yaml
+CLUSTER_CONFIG=../../../../cns/clusters/kscluster-ioni-qe-1.yaml
 REMOTE_PATH=/home/ec2-user/workDir/binaries/logGenerator
-NODE_NAME=va6-ioni-ksmaster-3
+NODE_NAME=va6-ioni-qe-1-ksmaster-3
 
 echo "Deploy in progress..."
 
