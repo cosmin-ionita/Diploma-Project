fb5b274ad611c89c4bcfd8097a3020c8a7d4793a
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-06-28 16:24:16 +0300

    Reshape log parser deploy script

--- SourceCode/DeployScripts/deploy_logParser.sh
+++ SourceCode/DeployScripts/deploy_logParser.sh
@@ -3,9 +3,9 @@
 ## This script deploys the log parser jar file to the cluster
 
 LOG_GEN_PATH=../LogParser/out/artifacts/Diploma_Project_jar/Diploma-Project.jar
-CLUSTER_CONFIG=../../../../cns/clusters/kscluster-ioni.yaml
+CLUSTER_CONFIG=../../../../cns/clusters/kscluster-ioni-qe-1.yaml
 REMOTE_PATH=/home/ec2-user/workDir/binaries/logParser
-NODE_NAME=va6-ioni-ksmaster-3
+NODE_NAME=va6-ioni-qe-1-ksmaster-3
 
 echo "Deploy in progress..."
 
