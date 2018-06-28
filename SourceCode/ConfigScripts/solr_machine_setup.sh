30492394be6d6e23fa35ca13efd32b3e0e2f7d2c
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-06-28 16:19:29 +0300

    Add solr machine setup

--- SourceCode/ConfigScripts/solr_machine_setup.sh
+++ SourceCode/ConfigScripts/solr_machine_setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+SCHEMA=./ConfigFiles/schema.xml
+CREATE_SOLR_CORE=./ConfigFiles/create_solr_core.sh
+DELETE_SOLR_CORE=./ConfigFiles/delete_solr_core.sh
 
+CLUSTER_CONFIG=../../../../cns/clusters/kscluster-ioni-qe-1.yaml
+REMOTE_PATH=/home/ec2-user/
+NODE_NAME=va6-ioni-qe-1-kssolr-1
+
+ops $CLUSTER_CONFIG sync $SCHEMA $NODE_NAME:$REMOTE_PATH -l ec2-user
+ops $CLUSTER_CONFIG sync $CREATE_SOLR_CORE $NODE_NAME:$REMOTE_PATH -l ec2-user
+ops $CLUSTER_CONFIG sync $DELETE_SOLR_CORE $NODE_NAME:$REMOTE_PATH -l ec2-user
+
+echo "Config done!"
