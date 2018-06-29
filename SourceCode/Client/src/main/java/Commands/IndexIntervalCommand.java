e36e85da70c2a8d2c89b4a0ba13b7c057e094d8f
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-06-29 13:56:59 +0300

    Add parameter validation

--- SourceCode/Client/src/main/java/Commands/IndexIntervalCommand.java
+++ SourceCode/Client/src/main/java/Commands/IndexIntervalCommand.java
@@ -9,7 +9,24 @@ public class IndexIntervalCommand implements Command {
     private String[] params;
 
     public void setParams(String[] params) throws IncorrectParameterException {
-       /* TODO check params here */
+
+       if(params.length == 1) {
+           if(params[0].contains("m")) {
+               int value = Integer.parseInt(params[0].split("m")[0]);
+
+               if(value < 1 || value > 59)
+                   throw new IncorrectParameterException("Incorrect number of minutes. It should be in the interval [1,59]");
+           } else if(params[0].contains("h")) {
+               int value = Integer.parseInt(params[0].split("h")[0]);
+
+               if(value < 1 || value > 12)
+                   throw new IncorrectParameterException("Incorrect number of hours. It should be in the interval [1,12]");
+           } else {
+               throw new IncorrectParameterException("Incorrect parameter");
+           }
+       } else {
+           throw new IncorrectParameterException("Incorrect number of parameters");
+       }
 
        this.params = params;
     }
