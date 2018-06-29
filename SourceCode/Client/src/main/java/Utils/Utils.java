ba86e749a2b81cb835e4b0ee6d14f1ef2617dbf2
Cosmin Ionita <ionita.cosmin@outlook.com>
2018-06-29 13:56:40 +0300

    Refactor code

--- SourceCode/Client/src/main/java/Utils/Utils.java
+++ SourceCode/Client/src/main/java/Utils/Utils.java
@@ -1,11 +1,18 @@
 package Utils;
 
 import Gui.GuiManager;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.io.FileUtils;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -16,6 +23,8 @@ import java.util.TimeZone;
 
 public class Utils {
 
+    private final static String bucketName = "aam-ioni-qe-1-test-logs";
+
     public static void showGUI() {
         Thread t = new Thread(new Runnable() {
             @Override
@@ -25,6 +34,14 @@ public class Utils {
         });
 
         t.start();
+    }
+
+    public static void wait(int miliseconds) {
+        try {
+            Thread.sleep(miliseconds);
+        }catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
     }
 
     private static String printUTCDate(String timeStamp) {
@@ -47,19 +64,6 @@ public class Utils {
         return null;
     }
 
-    public static void printSolrDocument(SolrDocument document) {
-        Collection<String> fields = document.getFieldNames();
-
-        for(String field : fields) {
-            if(field.equals("timeStamp"))
-                printUTCDate(document.get(field).toString());
-            else
-                System.out.print(document.get(field) + " ");
-        }
-
-        System.out.println("");
-    }
-
     public static String getDocumentsSerialized(SolrDocumentList documents) {
         String result = "";
 
@@ -77,6 +81,33 @@ public class Utils {
         }
 
         return result;
+    }
+
+    private static ByteArrayInputStream getByteArrayInputStream(File file) throws IOException {
+        return new ByteArrayInputStream(FileUtils.readFileToByteArray(file));
+    }
+
+    public static void uploadTestArchive(){
+        String archive = "/Users/ioni/Desktop/Diploma-Project-Repository/Diploma-Project-Repository/Diploma-Project/SourceCode/Client/src/test/TestResources/archive.zip";
+
+        try {
+            AmazonS3 client = AmazonS3ClientBuilder.defaultClient();
+
+            File file = new File(archive);
+
+            ByteArrayInputStream stream = getByteArrayInputStream(file);
+
+            ObjectMetadata metadata = new ObjectMetadata();
+
+            metadata.setContentLength(file.length());
+
+            metadata.setContentType("application/octet-stream");
+
+            client.putObject(bucketName, file.getName(), stream, metadata);
+
+        }catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public static Options buildOptions() {
@@ -162,13 +193,13 @@ public class Utils {
                 .desc(StringsMapping.indexNowDescription)
                 .build();
 
-        Option init = Option.builder(null)
-                .required(false)
-                .hasArg(false)
-                .numberOfArgs(0)
-                .longOpt(StringsMapping.initCommand)
-                .desc(StringsMapping.initCommandDescription)
-                .build();
+//        Option init = Option.builder(null)
+//                .required(false)
+//                .hasArg(false)
+//                .numberOfArgs(0)
+//                .longOpt(StringsMapping.initCommand)
+//                .desc(StringsMapping.initCommandDescription)
+//                .build();
 
         options.addOption(queryOption);
         options.addOption(timeOption);
